567c6746e0113e4d923252864a728bddf43ae37f
BibekPariyar <58519244+BibekPariyar10@users.noreply.github.com>
2020-11-30 17:52:06 +0545

    Add files via upload

---
+++ Spawn/Headers/DEFINITION.hpp
@@ -0,0 +1,30 @@
+//This header includes all the constants used in the program and filepath for different resources
+
+#pragma once
+
+//Game Window Properties
+#define WINDOW_WIDTH 960
+#define WINDOW_HEIGHT 540
+
+//Game Frame Rate
+#define GAME_FRAME_RATE 60
+
+//Splash State Properties
+#define SPLASH_STATE_DURATION 3.0f
+#define SPLASH_STATE_BACKGROUND_FILEPATH "Assets/Graphics/SplashBackground.png"
+
+//Main Menu State Properties
+#define MAIN_MENU_BACKGROUND_FILEPATH "Assets/Graphics/Background.png"
+#define MAIN_MENU_TITLE_FILEPATH "Assets/Graphics/Title.png"
+#define PLAY_BUTTON_FILEPATH "Assets/Graphics/PlayButton.png"
+#define PLAY_BUTTON_HOVER_FILEPATH "Assets/Graphics/PlayButtonHover.png"
+#define QUIT_BUTTON_FILEPATH "Assets/Graphics/QuitButton.png"
+#define QUIT_BUTTON_HOVER_FILEPATH "Assets/Graphics/QuitButtonHover.png"
+
+//Font properties
+#define FONT_FILEPATH "Assets/Fonts/MarkerFelt.ttf"
+
+//Game State Properties
+#define GAME_BACKGROUND_FILEPATH "Assets/Graphics/Background.png"
+#define CAR_SPRITE_FILEPATH "Assets/Graphics/rect.png"
+#define CAR_SPAWN_FREQUENCY 1.0f
